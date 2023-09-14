8e5e90f4d4d87db57417f1c67024b943f35a1b68
mogami <ndarfc.57@gmail.com>
2023-09-14 22:05:59 +0900

    add Translated to Japanese using ja.yml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      auto_login(@user) 
+      auto_login(@user)
       redirect_to '/', success: 'ユーザー登録が完了しました'
     else
       render :new, status: :unprocessable_entity
-    end 
+    end
   end
 
   private
