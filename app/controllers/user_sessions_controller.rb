d0c341933bc0c47340e638dcfa3e00e33f0ad2a1
mogami <ndarfc.57@gmail.com>
2023-09-14 09:37:00 +0900

    add signup_form and header

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,11 @@
+class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+  end
+
+  def destroy
+    logout
+    redirect_to login_path, success: 'ログアウトしました', status: :see_other
+  end
+end
