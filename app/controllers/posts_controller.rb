6923af23011fa010d4d2534bb9a857748af87cfd
mogami <ndarfc.57@gmail.com>
2023-09-19 21:27:03 +0900

    add post/index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,18 @@ class PostsController < ApplicationController
   def index
     @posts =Post.with_attached_images.includes(:user).order(created_at: :desc)
   end
-  
+
   def new
     @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, success: '投稿しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,5 +23,10 @@ class PostsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, images:[])
   end
 end
