8d5cba88cc2734551cd47988fce388948327a81c
mogami <ndarfc.57@gmail.com>
2023-11-18 21:54:33 +0900

    [Fix]ルーティングのタイポを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
-  get 'likes/create'
-  get 'likes/destroy'
-  get 'comments/create'
-  get 'comments/show'
-  get 'comments/edit'
   root to: 'posts#index'
-
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'user_sessions#new'
@@ -15,11 +7,11 @@ Rails.application.routes.draw do
   delete '/logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[index] do
-    resource :relationships, only: %i[create destroy], module: :users
+    resource :relationship, only: %i[create destroy], module: :users
   end
 
   resources :posts do
     resources :comments, module: :posts
-    resource :like, only: %i[create, destroy], module: :posts
+    resource :like, only: %i[create destroy], module: :posts
   end
 end
