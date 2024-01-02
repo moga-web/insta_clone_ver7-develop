0f2d2c9305cd2960e30e704486bf0662dfd8033b
mogami <ndarfc.57@gmail.com>
2024-01-02 19:19:33 +0900

    [Fix]updateのparamsを修正

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -5,11 +5,17 @@ class Mypage::AccountsController < Mypage::BaseController
 
   def update
     @user = User.find(current_user.id)
-    if @user.update(account_params)
+    if @user.update(profile_params)
       redirect_to edit_mypage_account_path, notice: 'アカウント情報を更新しました'
     else
       flash.now[:alert] = '更新に失敗しました'
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:avatar, :email, :username)
   end
 end
